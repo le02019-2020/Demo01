c1703fe16667517a10984e383d26ca9e2cb2c7f0
leo2019-2020 <class.learningtrees@gmail.com>
2020-11-25 23:39:37 -0500

    added SeachPage

--- src/main/java/com/qa/pages/LogIn.java
+++ src/main/java/com/qa/pages/LogIn.java
@@ -5,5 +5,9 @@ public class LogIn {
 	public void logIn(){
 		System.out.println("Log In Page");
 	}
+	
+	public void loginSet(){
+		System.out.println("logInSET");
+	}
 
 }
---
+++ src/main/java/com/qa/pages/SeachPage.java
@@ -0,0 +1,11 @@
+package com.qa.pages;
+
+public class SeachPage {
+	
+	
+	
+	public void searchPage(){
+		System.out.println("Seach Page");
+	}
+
+}
